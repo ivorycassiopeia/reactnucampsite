06c83246cb0dd723384fc7c30149bb0bc58203ab
April Ray <aprilray1@gmail.com>
2024-09-30 23:59:39 -0400

    Cloned old repo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import CampsitesList from './features/campsites/CampsitesList';
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      hello nucamp!
-    </div>
-  );
+  return <div className="App">
+    <Header />
+      <CampsitesList />
+        <Footer />
+        </div>;
 }
 
 export default App;
